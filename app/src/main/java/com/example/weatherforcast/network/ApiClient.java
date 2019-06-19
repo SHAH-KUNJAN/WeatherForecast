260e77f15a6d42481151026d3608852d18028793
shah-kunjan <kunjan.shah@pay1.in>
2019-06-20 00:22:13 +0530

    network  add

--- app/src/main/java/com/example/weatherforcast/network/ApiClient.java
+++ app/src/main/java/com/example/weatherforcast/network/ApiClient.java
@@ -1,4 +1,21 @@
 package com.example.weatherforcast.network;
 
+import com.example.weatherforcast.constants.Constants;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
+
+    private static Retrofit mRetrofit;
+
+    public static Retrofit getRetrofitInstance() {
+        if (mRetrofit == null) {
+            mRetrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return mRetrofit;
+    }
 }
--- app/src/main/java/com/example/weatherforcast/network/ApiInterface.java
+++ app/src/main/java/com/example/weatherforcast/network/ApiInterface.java
@@ -1,4 +1,18 @@
 package com.example.weatherforcast.network;
 
+import com.example.weatherforcast.model.OpenWeatherData;
+import com.example.weatherforcast.model.WeatherData;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiInterface {
+
+    @GET("forecast?units=metric")
+    Call<WeatherData> getWeatherData(@Query("q") String city, @Query("appid") String appId);
+
+    @GET("weather?units=metric")
+    Call<OpenWeatherData> getOpenWeatherData(@Query("q") String city, @Query("appid") String appId);
+
 }
